dba91349fa79825f9b1b1d6b292d752a704c53f7
podyankee <podyankee@gmail.com>
2019-10-06 06:23:00 +0300

    top slider finished

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 // import './index.pug'
-import $ from 'jquery'
+import $ from 'jquery';
 //import 'fullpage.js/dist/jquery.fullpage.css'
-// import 'owl.carousel2/dist/assets/owl.theme.default.css'
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+import 'owl.carousel';
 //import scrolloverflow from 'fullpage.js/vendors/scrolloverflow'
 //import fullpage from 'fullpage.js'
 // import 'jquery-form-styler/dist/jquery.formstyler.theme.css'
-import './js/common'
-import 'normalize.css'
-import './sass/main.sass'
+import './js/common';
+import 'normalize.css';
+import './sass/main.sass';
 
 // import 'vue'
 // import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
--- src/js/common.js
+++ src/js/common.js
@@ -1,3 +1,28 @@
 $(document).ready(function() {
+	const headerSlider = $(".owl-carousel");
+
+	headerSlider.on('initialized.owl.carousel', function(e) {
+
+		$('.slide-controls-number__active').text(e.item.index + 1);
+		$('.slide-controls-number__total').text(e.item.count);
+	});
+
+	headerSlider.owlCarousel({
+		items: 1,
+		dots: false,
+		smartSpeed: 1000
+	});
+	$('#headerSliderLeft').click(function() {
+		headerSlider.trigger('prev.owl.carousel')
+	});
+	$('#headerSliderRight').click(function() {
+		headerSlider.trigger('next.owl.carousel')
+	});
+
+	headerSlider.on('changed.owl.carousel', function(e) {
+
+	$('.slide-controls-number__active').text(e.item.index + 1);
+	$('.slide-controls-number__total').text(e.item.count);
+	});
 
 	});
